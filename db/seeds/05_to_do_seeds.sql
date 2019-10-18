5761be7ecacea6c810dd6b30386951502e49a4e5
Tyler <t.mar92@gmail.com>
2019-10-18 21:38:36 +0000

    adds more to-do items

--- db/seeds/05_to_do_seeds.sql
+++ db/seeds/05_to_do_seeds.sql
@@ -2,4 +2,12 @@
 INSERT INTO to_do_items (user_id, category_id, title, description, status_id)
 VALUES (1, 1, 'watch movie', 'great', 1),
 (2, 2, 'eat movie', 'okay', 2),
-(2, 3, 'read harry p', 'wowza', 3);
+(3, 4, 'buy shoes', 'yeahyeah', 1),
+(3, 5, 'monkey around', 'okay', 2),
+(2, 5, 'sing song', 'fantastic', 1),
+(4, 1, 'watch mission unpossible', 'fantastic', 1),
+(5, 1, 'watch mission unpossible', 'real cool', 1),
+(1, 4, 'buy mission unpossible', 'real cool', 2),
+(1, 3, 'read lotsa books', 'really gonna', 3),
+(2, 3, 'read harry p', 'wowza', 3),
+(5, 2, 'eat shoes', 'ffff', 1);
