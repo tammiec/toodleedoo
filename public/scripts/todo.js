b3654fe6f01d25e9908610de65979ff0bcb090d7
Tammie Chung <tammie.chung11@gmail.com>
2019-10-20 21:58:05 +0000

    Add jQuery functionality to to-do list items

--- public/scripts/todo.js
+++ public/scripts/todo.js
@@ -1,6 +1,30 @@
 // jQuery code for manipulating the to-do items
+
+// creates HTML for each to-do list
+const createListCard = function(obj) {
+  return `
+  <div class='card-deck'>
+    <div class="card">
+      <h5 class="card-header">To ${category.name}</h5>
+      <div class="card-body">
+        <ul class="list-group list-group-flush"></ul>
+      </div>
+    </div>
+  `;
+};
+
 $(() => {
-  $('ul li').click(() => {
+  $('ul li').click(function() {
     $(this).toggleClass('checked');
+  })
+
+  $('ul li').dblclick(function() {
+    $(this).toggleClass('important');
+  })
+
+  $('ul li').append('<span>&#x2715</span>');
+
+  $('ul li span').click(function() {
+    $(this).parent().hide();
   })
 });
