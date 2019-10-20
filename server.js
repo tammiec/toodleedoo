0e8aca9b142c7927919af30a76e431df53859381
jomicm <singularity.jmcm@gmail.com>
2019-10-20 18:49:31 -0400

    Create back end management for user profile updates

--- server.js
+++ server.js
@@ -2,17 +2,18 @@
 require('dotenv').config();
 
 // Web server config
-const PORT       = process.env.PORT || 8080;
-const ENV        = process.env.ENV || "development";
-const express    = require("express");
-const bodyParser = require("body-parser");
-const sass       = require("node-sass-middleware");
-const morgan     = require('morgan');
-const passport   = require('passport');
-const session    = require('express-session');
-const flash      = require('connect-flash');
-const partials   = require('express-partials')
-const app        = express();
+const PORT            = process.env.PORT || 8080;
+const ENV             = process.env.ENV || "development";
+const express         = require("express");
+const bodyParser      = require("body-parser");
+const sass            = require("node-sass-middleware");
+const morgan          = require('morgan');
+const passport        = require('passport');
+const session         = require('express-session');
+const flash           = require('connect-flash');
+const partials        = require('express-partials')
+const methodOverride  = require('method-override');
+const app             = express();
 
 // PG database client/connection setup
 const { Pool } = require('pg');
@@ -28,6 +29,7 @@ require('./auth/passport');
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
+app.use(methodOverride('_method'));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/styles", sass({
