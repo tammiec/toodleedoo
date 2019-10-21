cbb0c07cec2e874e781a355a38252aacaed1fb9f
Tyler <t.mar92@gmail.com>
2019-10-21 14:53:58 +0000

    removes extra indentation

--- public/scripts/todo.js
+++ public/scripts/todo.js
@@ -29,31 +29,31 @@ $(() => {
   });
 
   const getCategoryBtn = $('#getCategoryBtn');
-    const inputTask = $('#inputTask');
+  const inputTask = $('#inputTask');
 
-    getCategoryBtn.on('click', (e) => {
+  getCategoryBtn.on('click', (e) => {
+    getCategory();
+  });
+
+  inputTask.keypress(function (e) {
+    if (e.which == 13) {
       getCategory();
-    });
-
-    inputTask.keypress(function (e) {
-      if (e.which == 13) {
-        getCategory();
-        return false;    //<---- Add this line
-      }
-    });
-    // AJAX GET - Get category from server
-    const getCategory = async () => {
-      try {
-        console.log(inputTask.val());
-        const cat = await $.ajax('/category?input=' + inputTask.val(), { method: 'GET' });
-        console.log(cat[0]);
-        console.log('#' + cat[0].key);
-        // console.log(lanes);
-        $('#' + cat[0].key).append(`<li class="list-group-item">${inputTask.val()}</li>`);
-        // alert(cat[0].title);
-        $('#inputTask').val('');
-      } catch (err) {
-        console.error(err);
-      }
-    };
+      return false;    //<---- Add this line
+    }
+  });
+  // AJAX GET - Get category from server
+  const getCategory = async () => {
+    try {
+      console.log(inputTask.val());
+      const cat = await $.ajax('/category?input=' + inputTask.val(), { method: 'GET' });
+      console.log(cat[0]);
+      console.log('#' + cat[0].key);
+      // console.log(lanes);
+      $('#' + cat[0].key).append(`<li class="list-group-item">${inputTask.val()}</li>`);
+      // alert(cat[0].title);
+      $('#inputTask').val('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
 });
