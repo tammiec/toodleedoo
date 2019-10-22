b71d3af9ba62c551c7608eb9a7d00b84039e8cdf
Tammie Chung <tammie.chung11@gmail.com>
2019-10-22 15:09:35 -0400

    Implement tab routing

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,20 +1,35 @@
-$(() => {
-  // let hash = window.location.hash;
+const showRegister = () => {
+  $("#login").removeClass('select');
+  $("#register").addClass('select');
+  $("#login-form").hide();
+  $("#register-form").show();
+};
 
+const showLogin = () => {
+  $("#register").removeClass('select');
+  $("#login").addClass('select');
+  $("#register-form").hide();
+  $("#login-form").show();
+};
+
+$(() => {
   $('#register-form').hide();
+
+  let hash = window.location.hash;
+  console.log(hash);
+
+  if (hash.length > 0) {
+    if (hash === '#register-form') {
+      showRegister();
+    }
+  };
 
   $(".tab").click(function() {
     var x = $(this).attr('id');
     if (x === 'register') {
-      $("#login").removeClass('select');
-      $("#register").addClass('select');
-      $("#login-form").hide();
-      $("#register-form").show();
+      showRegister();
     } else {
-      $("#register").removeClass('select');
-      $("#login").addClass('select');
-      $("#register-form").hide();
-      $("#login-form").show();
+      showLogin();
     };
   });
 });
