be24a39531643dcc7437198752b40176f85a8fe5
Tammie Chung <tammie.chung11@gmail.com>
2019-10-22 14:15:31 -0400

    Change styling - tabbed login form and minor css edits

---
+++ public/scripts/login.js
@@ -0,0 +1,17 @@
+$(() => {
+  $('#register-form').hide();
+  $(".tab").click(function() {
+    var x = $(this).attr('id');
+    if (x === 'register') {
+      $("#login").removeClass('select');
+      $("#register").addClass('select');
+      $("#login-form").hide();
+      $("#register-form").show();
+    } else {
+      $("#register").removeClass('select');
+      $("#login").addClass('select');
+      $("#register-form").hide();
+      $("#login-form").show();
+    };
+  });
+});
