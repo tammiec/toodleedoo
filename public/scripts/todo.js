eeeffec42830c6154fdebb5a9c90a1d2c0bb85bc
Tyler <t.mar92@gmail.com>
2019-10-23 22:03:18 +0000

    adds class checked, and sets img src to the checked.png on tasks loaded in the archived view

--- public/scripts/todo.js
+++ public/scripts/todo.js
@@ -169,7 +169,7 @@ const renderTasks = function(tasks) {
     } else if (task.status_id === 3) {
       $('#' + task.key).append(`
         <li class="list-group-item" id="task-${task.id}" class="draggable" draggable="true" ondragstart="drag(event)" data-toggle="modal" data-target="#exampleModal" data-task-name="${task.title}" data-task-desc="${task.description}">
-          <img class='checkbox' src="../images/not-checked.png">
+          <img class='checkbox checked' src="../images/checked.png">
           <span class='task-name'>${task.title}</span>
           <span class='x'>&#x2715</span>
           <span class='star'><img class="marked-important" src="${imgSrc}"></span>
